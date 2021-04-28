0af5770b0bc61d01c0301922a059dfb45b87bdb7
LewisLinn <LewisLinn@users.noreply.github.com>
2021-04-28 09:12:25 +0000

    deploy: LewisLinn/starter-academic@03db1c174ed6307492b0f5610b964908bf982f64

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = {highlight_style: "dracula", theme: "black"};
 
-  // ns-hugo:/var/folders/1p/sz2mcp6j2rj5nwwmdp5x16b00000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20210413192202-ab8d28988e0e/assets/js/wowchemy-utils.js
+  // ns-hugo:/var/folders/24/8k48jl6d249_n_qfxwsl6xvm0000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20210413192202-ab8d28988e0e/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
